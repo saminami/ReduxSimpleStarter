8114ae618bf5a9861e0438ecdc1200aafea3bad9
Sami Peralahti <sami.peralahti@gmail.com>
2017-06-11 18:24:43 +0300

    using the yt-api-search package to get data of videos

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, {Component} from "react";
 import ReactDOM from "react-dom";
-
+import YTSearch from "youtube-api-search"
 import SearchBar from "./components/search_bar";
-
 const API_KEY = "AIzaSyB4zLuS4v4y577DnhNq7CxiVT7YftUV8ws";
 
 // Create a new component. This component should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: "surfboards"}, (videos) =>{
+      // this.setState({videos : videos});
+      this.setState({ videos });
+    });
+  }
+
+  render(){
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 
