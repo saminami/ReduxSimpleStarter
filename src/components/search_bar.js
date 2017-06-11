6d917570cd15a0217343068e0decce364605d36c
Sami Peralahti <sami.peralahti@gmail.com>
2017-06-11 15:38:04 +0300

    worked on the basics

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 
-class SearchBar extends React.Component {
-  
+class SearchBar extends Component {
+
+    constructor(props){
+      super(props);
+
+      this.state = { term: "" };
+    }
+
+    render(){
+      return (
+        <div>
+          <input
+          value = {this.state.term}
+          onChange={event => this.setState({ term: event.target.value })} />
+          Value of the input: {this.state.term}
+        </div>
+      );
+    }
+
 }
 
 export default SearchBar;
